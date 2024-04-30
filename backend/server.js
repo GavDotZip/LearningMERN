4b3e947b51690dde65d028ca3187fdc5b1c350d4
GavDotZip <gavdotzip@gmail.com>
2024-04-30 23:29:09 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 // Configure an Express Server
 import express from "express"
-import cores from "cores"
+import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
- 
+ const app = express()
+
+ // Apply Middleware
+ app.use(cors())
+ app.use(express.json())
+
+ //  Specify Initial Routes
+ app.use("/api/v1/restaurants", restaurants)
+ app.use("*", (req, res) => res.status(404).json({ error: "not found"})) // 404 if different route
+
+ // Export app as module
+ export default app
